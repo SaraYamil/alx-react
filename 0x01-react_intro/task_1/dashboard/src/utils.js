52c5c6a3beb8b457d1becd76286c685757c6235e
sara yamil <sarayamil18@gmail.com>
2024-03-06 21:21:01 +0000

    me

--- 0x01-react_intro/task_1/dashboard/src/utils.js
+++ 0x01-react_intro/task_1/dashboard/src/utils.js
@@ -1,18 +1,3 @@
+export const getFullYear = () => new Date().getFullYear();
 
-const getFullYear = () => {
-	let current_year = new Date().getFullYear();
-	return current_year;
-}
-
-const getFooterCopy = (isIndex) => {
-	if (isIndex) {
-		return "Holberton School";
-	} else {
-		return "Holberton School main dashboard";
-	}
-}
-
-module.exports = {
-	getFooterCopy,
-	getFullYear,
-};
+export const getFooterCopy = (isIndex) => (isIndex ? "Holberton School" : "Holberton School main dashboard");
